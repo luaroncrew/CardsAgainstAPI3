e7bd9912579fb4d06663e79c0bed2b0988cb66bd
stupside <kilian.houpeurt@gmail.com>
2024-02-05 20:12:32 +0100

    remove tsignore

--- backend/src/utils/QRNG.ts
+++ backend/src/utils/QRNG.ts
@@ -1,5 +1,3 @@
-
-
 import Web3 from "web3";
 
 const providerUrl = "https://replicator-01.pegasus.lightlink.io/rpc/v1";
@@ -7,179 +5,181 @@ const contractAddress = "0xFaCF1FDf1eE5D0e03AEFdB2511ced41d3F713446";
 
 const contractABI = [
   {
-    "constant": true,
-    "inputs": [],
-    "name": "airnode",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "airnode",
+    outputs: [
       {
-        "name": "",
-        "type": "address"
-      }
+        name: "",
+        type: "address",
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "endpointIdUint256",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "endpointIdUint256",
+    outputs: [
       {
-        "name": "",
-        "type": "bytes32"
-      }
+        name: "",
+        type: "bytes32",
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "sponsorWallet",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "sponsorWallet",
+    outputs: [
       {
-        "name": "",
-        "type": "address"
-      }
+        name: "",
+        type: "address",
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "constant": true,
-    "inputs": [],
-    "name": "qrngUint256",
-    "outputs": [
+    constant: true,
+    inputs: [],
+    name: "qrngUint256",
+    outputs: [
       {
-        "name": "",
-        "type": "uint256"
-      }
+        name: "",
+        type: "uint256",
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "constant": true,
-    "inputs": [
+    constant: true,
+    inputs: [
       {
-        "name": "",
-        "type": "bytes32"
-      }
+        name: "",
+        type: "bytes32",
+      },
     ],
-    "name": "expectingRequestWithIdToBeFullfilled",
-    "outputs": [
+    name: "expectingRequestWithIdToBeFullfilled",
+    outputs: [
       {
-        "name": "",
-        "type": "bool"
-      }
+        name: "",
+        type: "bool",
+      },
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+    payable: false,
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "name": "requestId",
-        "type": "bytes32"
-      }
+        indexed: true,
+        name: "requestId",
+        type: "bytes32",
+      },
     ],
-    "name": "RequestUint256",
-    "type": "event"
+    name: "RequestUint256",
+    type: "event",
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "name": "requestId",
-        "type": "bytes32"
+        indexed: true,
+        name: "requestId",
+        type: "bytes32",
       },
       {
-        "indexed": false,
-        "name": "response",
-        "type": "uint256"
-      }
+        indexed: false,
+        name: "response",
+        type: "uint256",
+      },
     ],
-    "name": "ReceivedUint256",
-    "type": "event"
+    name: "ReceivedUint256",
+    type: "event",
   },
   {
-    "constant": false,
-    "inputs": [
+    constant: false,
+    inputs: [
       {
-        "name": "_airnode",
-        "type": "address"
+        name: "_airnode",
+        type: "address",
       },
       {
-        "name": "_endpointIdUint256",
-        "type": "bytes32"
+        name: "_endpointIdUint256",
+        type: "bytes32",
       },
       {
-        "name": "_sponsorWallet",
-        "type": "address"
-      }
+        name: "_sponsorWallet",
+        type: "address",
+      },
     ],
-    "name": "setRequestParameters",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: "setRequestParameters",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "constant": false,
-    "inputs": [],
-    "name": "makeRequestUint256",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
+    constant: false,
+    inputs: [],
+    name: "makeRequestUint256",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
   },
   {
-    "constant": false,
-    "inputs": [
+    constant: false,
+    inputs: [
       {
-        "name": "requestId",
-        "type": "bytes32"
+        name: "requestId",
+        type: "bytes32",
       },
       {
-        "name": "data",
-        "type": "bytes"
-      }
+        name: "data",
+        type: "bytes",
+      },
     ],
-    "name": "fulfillUint256",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: "fulfillUint256",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const readPublicVariable = async (): Promise<number> => {
+  // Create a Web3 instance using the Infura provider
+  const web3 = new Web3(providerUrl);
+
+  // Create a contract instance
+  const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+  // Call the contract function to read the public variable
+
+  if (contract.methods.qrngUint256 === undefined) {
+    throw new Error("qrngUint256 is undefined");
   }
-]
 
+  const method = contract.methods.qrngUint256();
 
-async function readPublicVariable() {
-  try {
-    // Create a Web3 instance using the Infura provider
-    const web3 = new Web3(providerUrl);
+  const response = await method.call();
 
-    // Create a contract instance
-    const contract = new web3.eth.Contract(contractABI, contractAddress);
+  // TODO: Convert the response to a number
+  return response;
+};
 
-    // Call the contract function to read the public variable
-    // @ts-ignore
-    return await contract.methods.qrngUint256().call();
-
-  } catch (error) {
-    console.error("Error reading public variable:", error);
-  }
-}
-
-
-export async function getRandomNumber() {
+export const getRandomNumber = async () => {
   return await readPublicVariable();
-}
+};
